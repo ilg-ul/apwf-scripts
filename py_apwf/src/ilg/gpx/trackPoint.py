f33049bda240879a4a99572a9612fb83dc27e6b6
Liviu Ionescu <ilg@livius.net>
2012-07-11 21:48:00 +0300

    conversions from strings added

--- py_apwf/src/ilg/gpx/trackPoint.py
+++ py_apwf/src/ilg/gpx/trackPoint.py
@@ -1,14 +1,60 @@
 
+from datetime import datetime
+from ilg.apwf.gmtTzinfo import ZuluTzinfo
+
 class TrackPoint(object):
 
-    def __init__(self, latitude, longitude, elevation, timestamp):
+    def __init__(self, latitude, longitude, elevation = None, timestamp = None):
 
-        self.latitude = latitude
-        self.longitude = longitude
-        self.elevation = elevation
-        self.timestamp = timestamp
+        self.latitude = float(latitude)
+        self.longitude = float(longitude)
+        
+        if elevation == None:
+            self.elevation = elevation
+        else:
+            self.elevation = float(elevation)
+        
+        self.tzUtc = ZuluTzinfo()
+
+        if timestamp == None:
+            self.timestamp = None
+        else:
+            self.timestamp = self._convertStringToDate(timestamp)
+        
         
         return
+    
+    
+    def setElevation(self, elevation):
+        
+        if elevation == None:
+            self.elevation = None
+        else:
+            self.elevation = float(elevation)
+        
+        return
+    
+    
+    def setTimestamp(self, timestampString):
+        
+        if timestampString == None:
+            self.timestamp = None
+        else:
+            self.timestamp = self._convertStringToDate(timestampString)   
+                
+        return
+    
+    
+    def _convertStringToDate(self, timestampString):
+    
+        try:
+            timestamp = datetime.strptime(timestampString, '%Y-%m-%dT%H:%M:%SZ')        
+            timestampWithTz = self.tzUtc.localize(timestamp)
+        except ValueError as err:
+            print err
+            timestampWithTz = None
+        
+        return timestampWithTz
     
 
     
